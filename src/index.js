bb4093684de18f76b4921c5f2919edfe2021acf8
NosiphoNdzinisa <nosiphondzinisa@gmail.com>
2025-07-02 22:52:33 +0200

    inject the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -45,7 +45,33 @@ function handleSearchSubmit(event) {
 searchCity(searchInput.value);
 }
 
+function displayForecast () {
+  let forecastHtml = "";
+  let days = ['Thur', 'Fri', 'Sat', 'Sun', 'Mon'];
+
+  days.forEach(function (day) {
+  forecastHtml = forecastHtml +
+   `
+  <div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">🌤</div>
+  <div class="weather-forecast-temperatures">
+  <div class="weather-forecast-temperature">
+  <strong>15&deg</strong>
+  </div>
+  <div class="weather-forecast-temperature">9&deg</div>
+  </div>
+  </div>
+`;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit)
 
 searchCity("Paris")
+displayForecast ();
+
