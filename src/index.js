81058ff6851404837376779396c1a427afbee714
NosiphoNdzinisa <nosiphondzinisa@gmail.com>
2025-07-03 22:43:14 +0200

    updated project files

--- src/index.js
+++ src/index.js
@@ -47,6 +47,13 @@ function handleSearchSubmit(event) {
 searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast (city) {
 let apikey = "46eb1514fecb5a730ct10b7d5o64c5f7";
 let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
@@ -54,23 +61,30 @@ axios(apiUrl).then(displayForecast);
 }
 
 function displayForecast (response) {
-  let forecastHtml = "";
-  let days = ['Thur', 'Fri', 'Sat', 'Sun', 'Mon'];
+  console.log(response.data);
 
-  days.forEach(function (day) {
-  forecastHtml = forecastHtml +
-   `
+  let forecastHtml = "";
+ 
+ response.data.daily.forEach(function (day,index) {
+  if (index < 5){
+  forecastHtml =
+  forecastHtml +
+    `
   <div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">🌤</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  <img src= "${day.condition.icon_url}" class="weather-forecast-icon" /> 
+  </div>
   <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
-  <strong>15&deg</strong>
+  <strong>${Math.round(day.temperature.maximum)}&deg</strong>
   </div>
-  <div class="weather-forecast-temperature">9&deg</div>
+  <div class="weather-forecast-temperature">${Math.round(
+    day.temperature.minimum
+  )}&deg</div>
   </div>
   </div>
 `;
+  }
 });
 
 let forecastElement = document.querySelector("#forecast");
